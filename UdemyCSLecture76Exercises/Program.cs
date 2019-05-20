4ab22dbc5924e4b37dd53c7dbe9382199be06ea2
Marcus Mayo <marcus.mayo@medhub.com>
2019-05-19 22:13:36 -0500

    Problem 1 complete

--- UdemyCSLecture76Exercises/Program.cs
+++ UdemyCSLecture76Exercises/Program.cs
@@ -10,6 +10,25 @@ namespace UdemyCSLecture76Exercises
     {
         static void Main(string[] args)
         {
+            /*
+             * * #1- Write a program that reads a text file and displays the number of words.
+             */
+
+            var path = @"c:\cSharpFileTest\";
+            string text = File.ReadAllText(path + "cSharpTestFile.txt");
+            string[] splitStr = text.Split(new[] { "\r\n", "\r", "\n", " " }, StringSplitOptions.None);
+            int wordCount = 0;
+
+            foreach (var word in splitStr)
+            {
+                Console.Write(word + ' ');
+                wordCount += 1;
+            }
+            Console.WriteLine("\nnumber of words: " + wordCount);
+
+            /*
+             * 
+             */
         }
     }
 }
